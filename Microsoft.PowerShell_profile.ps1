979b8d18f2123c006f2f5461eab9eeaa405428fb
rafaeloledo <rafaeloliveiraledo@gmail.com>
2024-01-14 11:13:38 -0300

    chore(pwsh): update config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,7 +4,9 @@
 $PSDefaultParameterValues['Out-File:Encoding'] = 'utf8'
 $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 
-Import-Module Terminal-Icons # Install-Module -Name Terminal-Icons
+Import-Module Terminal-Icons
+Import-Module PSFzf
+Import-Module PsFzfUtil
 
 function Export ($PathToAdd) {
 	$existingPath = [Environment]::GetEnvironmentVariable('Path', 'User')
@@ -16,20 +18,24 @@ function GlobalExport ($PathToAdd) {
 	($existingPath -notlike "*$PathToAdd*") -and [Environment]::SetEnvironmentVariable('Path', $env:Path + ";" + $PathToAdd, 'Machine')
 }
 
-Export 'C:\Users\rafae\scoop\apps\scoop\current\bin' > $null
-Export 'C:\Users\rafae\scoop\shims' > $null
+# Just for registering the Exports, run this only one time in the cli
+# Export 'C:\scoop\apps\scoop\current\bin' > $null
+# Export 'C:\Users\rafae\AppData\Local\Obsidian' > $null
+# Export 'C:\scoop\shims' > $null
 
-# Needs admin privileges
-# Runs only if the varible not exist
-$env:GIT_EDITOR -or [Environment]::SetEnvironmentVariable('GIT_EDITOR', 'C:\\Users\\rafae\\scoop\\shims\\nvim.exe', 'Machine') >$null
-$env:SCOOP -or [Environment]::SetEnvironmentVariable('SCOOP', 'C:\Users\rafae\scoop' , 'Machine') >$null
-$env:SCOOP_GLOBAL -or [Environment]::SetEnvironmentVariable('SCOOP_GLOBAL', 'C:\Users\rafae\scoopg', 'Machine') >$null
-
-Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 
+Remove-Alias cat 2>&1>$null
+function cat ($parameter) {
+	bat --plain --color=always $parameter
+}
+
+# git pull on all children repos
 function pall () {
 	$dirs = Get-ChildItem -Path . | Where-Object { $_.PSIsContainer }
 	$back = Get-Location
@@ -41,6 +47,7 @@ function pall () {
 	Set-Location $back.Path
 }
 
+# git status from all children repos
 function st () {
 	$originalDir = Get-Location
 	$dirs = Get-ChildItem -Path . | Where-Object { $_.PSIsContainer }
@@ -53,49 +60,58 @@ function st () {
 	Set-Location $originalDir
 }
 
-function sup () {
-	scoop update *
-	scoop update bucket *
-	scoop cleanup *
-	scoop status
+function celular () {
+  scrcpy --disable-screensaver --turn-screen-off
 }
 
-function prompt {
-  $Host.UI.RawUI.WindowTitle = ($pwd | Split-Path -Leaf)
-	$(if (Test-Path variable:/PSDebugContext) { '[DBG]: ' }
-		else { '' }) + $(Split-Path -Path (Get-Location) -Leaf) +
-	$(if ($NestedPromptLevel -ge 1) { '>>' }) + ' 󰁕 '
+function tree {
+  eza --tree --git-ignore
 }
 
-Set-Alias v nvim
-Set-Alias vim nvim
-Set-Alias which where.exe
-Set-Alias mv Rename-Item
-Set-Alias touch New-Item
-Set-Alias scmds Show-Command
+Set-Alias vim 'C:\scoop\apps\neovim\current\bin\nvim.exe'
+Set-Alias v 'C:\scoop\apps\neovim\current\bin\nvim.exe'
 Set-Alias g git
-Set-Alias grep findstr
-Set-Alias tig 'tig.exe'
-Set-Alias less 'less.exe'
-Set-Alias mv Move-Item
-Set-Alias rn Rename-Item
+Set-Alias e emacs
+Set-Alias s 'subl.exe'
+Set-Alias obs 'Obsidian.exe'
+Set-Alias reboot 'shutdown /r'
+Set-Alias xxd 'C:\scoop\apps\git\current\usr\bin\xxd.exe'
+Set-Alias touch 'C:\scoop\apps\git\current\usr\bin\touch.exe'
+Set-Alias rm 'C:\scoop\apps\git\current\usr\bin\rm.exe'
+Set-Alias bash 'C:\scoop\apps\git\current\usr\bin\bash.exe'
+Set-Alias grep 'C:\scoop\apps\git\current\usr\bin\grep.exe'
+Set-Alias tig 'C:\scoop\apps\git\current\usr\bin\tig.exe'
+Set-Alias less 'C:\scoop\apps\git\current\usr\bin\less.exe'
+Set-Alias awk 'C:\scoop\apps\git\current\usr\bin\awk.exe'
+Set-Alias mv 'C:\scoop\apps\git\current\usr\bin\mv.exe'
+Set-Alias perl 'C:\scoop\apps\git\current\usr\bin\perl.exe'
+Set-Alias xargs 'C:\scoop\apps\git\current\usr\bin\xargs.exe'
+
+function which ($command) {
+  Get-Command $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
 
 function ll {
-  Get-ChildItem -Force $args[0]
+  eza -lga --icons
 }
 
 Set-PSReadLineKeyHandler -Chord "Alt+l" -Function AcceptSuggestion
 Set-PSReadLineKeyHandler -Chord "Ctrl+LeftArrow" -Function BackwardWord
+# Set-PSReadLineKeyHandler -Chord "Alt+LeftArrow" -Function BackwardWord
 Set-PSReadLineKeyHandler -Chord "Ctrl+RightArrow" -Function NextWord
+# Set-PSReadLineKeyHandler -Chord "Alt+RightArrow" -Function NextWord
+Set-PSReadLineKeyHandler -Chord "Ctrl+Backspace" -Function BackwardKillWord
 Set-PSReadLineKeyHandler -Chord "Ctrl+a" -Function SelectAll
-Set-PSReadLineKeyHandler -Chord "Alt+w" -Function KillWord
-Set-PSReadLineKeyHandler -Chord "Ctrl+Alt+d" -Function KillLine
+Set-PSReadLineKeyHandler -Chord "Ctrl+o" -ScriptBlock { MyFzf } > $null
+Set-PSReadLineKeyHandler -Chord "Ctrl+r" -ScriptBlock { MyRg  } > $null
 
 function ide {
-  start wt 'sp -H -s 0.30'
+  Start-Process wt "sp -H -s 0.30 -d $PWD"
   Start-Sleep -Seconds 0.2
-  start wt 'sp -V -s 0.65'
+  Start-Process wt "sp -V -s 0.65 -d $PWD"
   Start-Sleep -Seconds 0.2
-  start wt 'sp -V -s 0.5'
-  Start-Sleep -Second 0.2
+  Start-Process wt "sp -V -s 0.5 -d $PWD"
+  Start-Sleep -Seconds 0.2
 }
+
+oh-my-posh init pwsh -c C:\Users\rafae\OneDrive\Documentos\PowerShell\rafaeloledo.omp.json | Invoke-Expression
